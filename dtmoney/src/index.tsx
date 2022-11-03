9363e36ed438f8369c8ea93b296edc141f49d19e
murilo muller <murilomuller@protonmail.com>
2022-11-03 16:09:29 -0300

    att arquivos pai

--- dtmoney/src/index.tsx
+++ dtmoney/src/index.tsx
@@ -46,4 +46,8 @@ createServer({
 })
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<App />)
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
