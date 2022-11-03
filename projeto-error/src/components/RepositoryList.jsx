31897062137eeb8e6710fd22942e0c4de7666b06
murilo muller <murilomuller@protonmail.com>
2022-11-03 19:33:14 -0300

    ajustes sobre o codigo antigo

--- projeto-error/src/components/RepositoryList.jsx
+++ projeto-error/src/components/RepositoryList.jsx
@@ -1,21 +1,35 @@
 import { RepositoryItem } from "./RepositoryItem"
+import '../styles/repositories.js'
+import { Container } from "../styles/repositories.js"
+import { useEffect, useState } from "react"
 
 const repoName = {
-  name: "batata-frita",
-  description: "batata-cozida",
-  link: "https://github.com.br"
+  name: "unform",
+  description: "Forms in React",
+  link: "https://github.com/unform/unform"
 }
 
-
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  })
+
   return (
-    <section className="repository-list">
-      <h1>Lista de repositorios</h1>
+    <Container>
+      <section className="repository-list">
+        <h1>Lista de repositorios</h1>
 
-      <RepositoryItem
-        repoName={repoName}
-      />
+        <ul>
+          {repositories.map(repository => {
+            return <RepositoryItem key={repository.name} repoName={repository} />
+          })}
+        </ul>
 
-    </section>
+      </section>
+    </Container>
   )
 }
