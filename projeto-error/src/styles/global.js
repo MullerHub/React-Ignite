0a7c82f7b93b5accf5d68fe2156e99cce06e53d2
murilo muller <murilomuller@protonmail.com>
2022-11-03 19:33:10 -0300

    ajustes sobre o codigo antigo

--- projeto-error/src/styles/global.js
+++ projeto-error/src/styles/global.js
@@ -1,3 +1,6 @@
+import { createGlobalStyle } from 'styled-components'
+export default createGlobalStyle`
+
 * {
   margin: 0;
   padding: 0;
@@ -7,3 +10,4 @@
 .repository-list {
   background-color: red($color: #ff0000);
 }
+`
---
+++ projeto-error/src/styles/repositories.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  section.repository-list {
+    margin: 40px;
+
+    h1 {
+      margin-bottom: 20px;
+    }
+    list-style: none;
+  }
+`
