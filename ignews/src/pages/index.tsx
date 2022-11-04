a4420f19ab006da1e09ce9de43fb27b599781fd5
murilo muller <murilomuller@protonmail.com>
2022-11-04 18:56:27 -0300

    add pages

---
+++ ignews/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Homi</title>
+      </Head>
+      <h1>Hello world</h1>
+    </>
+  )
+}
