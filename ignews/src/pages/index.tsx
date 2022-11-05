af7c332b47dc62e5bef8a52eb37c4d17b937c257
murilo muller <murilomuller@protonmail.com>
2022-11-05 17:19:28 -0300

    finished pages

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -2,8 +2,17 @@ import Head from 'next/head'
 import React from 'react'
 import { SubscribeButton } from '../components/SubscribeButton'
 import styles from './home.module.scss'
+import { GetStaticProps } from 'next'
+import { stripe } from '../services/stripe'
 
-export default function Home() {
+interface IHomeProps {
+  product: {
+    priceId: string
+    amount: number
+  }
+}
+
+export default function Home({ product }: IHomeProps) {
   return (
     <>
       <Head>
@@ -18,9 +27,9 @@ export default function Home() {
           </h1>
           <p>
             Get acess to all the publications <br />
-            <span>for $1.93 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
@@ -28,3 +37,24 @@ export default function Home() {
     </>
   )
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1M0qs3AOshFAsJOxMogVBKNj')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
+  }
+}
+
+// ERROR INVALID API KEY, ERRO NA CHAVE DA API DA STRIPE.
