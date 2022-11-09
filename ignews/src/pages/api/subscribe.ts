3b30ff779696e17f748927893f9dee3ec6a096c0
murilo muller <murilomuller@protonmail.com>
2022-11-09 16:17:07 -0300

    Refactor: ajustes no codigo

--- ignews/src/pages/api/subscribe.ts
+++ ignews/src/pages/api/subscribe.ts
@@ -26,7 +26,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (!customerId) {
       const stripeCustomer = await stripe.customers.create({
         email: session.user.email
-        // metadata:
       })
 
       await fauna.query(
@@ -41,17 +40,17 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     }
 
     try {
-      const checkoutSession = await stripe.checkout.sessions.create({
+      const stripeCheckoutSession = await stripe.checkout.sessions.create({
         customer: customerId,
         payment_method_types: ['card'],
         billing_address_collection: 'required',
         line_items: [{ price: 'price_1M0qs3AOshFAsJOxMogVBKNj', quantity: 1 }],
         mode: 'subscription',
         allow_promotion_codes: true,
-        success_url: process.env.STRIPE_SUCESS_URL,
+        success_url: process.env.STRIPE_SUCCESS_URL,
         cancel_url: process.env.STRIPE_CANCEL_URL
       })
-      return res.status(200).json({ sessionId: checkoutSession.id })
+      return res.status(200).json({ sessionId: stripeCheckoutSession.id })
     } catch (error) {
       console.log('ERRO DA CHEKOUT:', error)
     }
