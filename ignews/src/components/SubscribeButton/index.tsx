c8d58ce4d441429e5ab51b0c47795931fa328ad7
murilo muller <murilomuller@protonmail.com>
2022-11-08 22:28:37 -0300

    necessita revisão

--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -1,11 +1,45 @@
+import { useSession, signIn } from 'next-auth/react'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
 import styles from './styles.module.scss'
 
 interface ISubscribeButtonProps {
   priceId: string
 }
+
 export function SubscribeButton({ priceId }: ISubscribeButtonProps) {
+  const { data: session } = useSession()
+
+  // session.user
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return
+    }
+    try {
+      const response = await api
+        .post('/subscribe')
+        .then(data => data.data)
+        .catch(error => console.log(error))
+      const { sessionId } = response
+      console.log('RESPONSE CHEGANDO:', response)
+
+      console.log('SESSION chegando:', sessionId)
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (err) {
+      alert(err.message)
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   )
