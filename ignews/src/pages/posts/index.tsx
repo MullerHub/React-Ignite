d672a62777d93012f05d9c39379559c5054129ea
Murilo Muller <murilomuller@protonmail.com>
2022-11-12 19:35:04 -0300

    posts add

--- ignews/src/pages/posts/index.tsx
+++ ignews/src/pages/posts/index.tsx
@@ -1,7 +1,21 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
+import { getPrismicClient } from '../../services/prismic'
 import styles from './styles.module.scss'
+import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
 
-export default function Posts() {
+type Posts = {
+  slug: string
+  title: string
+  excerpt: string
+  updatedAt: string
+}
+interface IPostsProps {
+  posts: Posts[]
+}
+
+export default function Posts({ posts }: IPostsProps) {
   return (
     <>
       <Head>
@@ -9,51 +23,51 @@ export default function Posts() {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>11 de novembro de 2022 </time>
-            <strong> Criado por Mullerhub</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
-              reprehenderit sapiente? Quas veritatis consectetur id quos libero
-              eum, est sed, illo autem, architecto quia quisquam deleniti velit.
-              Iusto, eveniet repudiandae?
-            </p>
-          </a>
-
-          <a href="#">
-            <time>11 de novembro de 2022 </time>
-            <strong> Criado por Mullerhub</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
-              reprehenderit sapiente? Quas veritatis consectetur id quos libero
-              eum, est sed, illo autem, architecto quia quisquam deleniti velit.
-              Iusto, eveniet repudiandae?
-            </p>
-          </a>
-
-          <a href="#">
-            <time>11 de novembro de 2022 </time>
-            <strong> Criado por Mullerhub</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
-              reprehenderit sapiente? Quas veritatis consectetur id quos libero
-              eum, est sed, illo autem, architecto quia quisquam deleniti velit.
-              Iusto, eveniet repudiandae?
-            </p>
-          </a>
-
-          <a href="#">
-            <time>11 de novembro de 2022 </time>
-            <strong> Criado por Mullerhub</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
-              reprehenderit sapiente? Quas veritatis consectetur id quos libero
-              eum, est sed, illo autem, architecto quia quisquam deleniti velit.
-              Iusto, eveniet repudiandae?
-            </p>
-          </a>
+          {posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+
+  const response = await prismic.query(
+    [Prismic.predicates.at('document.type', 'publication')],
+    {
+      fetch: ['publication.title', 'publication.content'],
+      pageSize: 20
+    }
+  )
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find(content => content.type === 'paragraph')?.text ??
+        '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        'pt-BR',
+        {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        }
+      )
+    }
+  })
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
