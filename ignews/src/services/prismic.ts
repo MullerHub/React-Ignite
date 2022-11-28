beca6d71ef45a13cf5577ea6a2387848a6b1b955
Murilo Muller <murilomuller@protonmail.com>
2022-11-28 18:59:24 -0300

    ajustes ignews

--- ignews/src/services/prismic.ts
+++ ignews/src/services/prismic.ts
@@ -1,10 +1,40 @@
-import Prismic from '@prismicio/client'
+import * as prismic from '@prismicio/client'
+import { enableAutoPreviews } from '@prismicio/next'
+import { NextApiRequest, PreviewData } from 'next'
+import sm from '../../sm.json'
 
-export function getPrismicClient(req?: unknown) {
-  const prismic = Prismic.client(process.env.PRISMIC_ENDPOINT, {
-    req,
+interface IPrismicContext {
+  req?: NextApiRequest
+  previewData: PreviewData
+}
+
+interface IPrismicResolver {
+  [key: string]: any
+}
+
+export const endpoint = sm.apiEndpoint
+export const repositoryName = prismic.getRepositoryName(endpoint)
+
+export function linkResolver(doc: IPrismicResolver) {
+  switch (doc.type) {
+    case 'posts':
+      return `/${doc.uid}`
+    default:
+      return null
+  }
+}
+
+export function createClient(config: IPrismicContext) {
+  const client = prismic.createClient(endpoint, {
+    ...config,
     accessToken: process.env.PRISMIC_ACCESS_TOKEN
   })
 
-  return prismic
+  enableAutoPreviews({
+    client,
+    previewData: config.previewData,
+    req: config.req
+  })
+
+  return client
 }
