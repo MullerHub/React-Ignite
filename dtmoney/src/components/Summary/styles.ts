5a4a69e0c1352f791036810a2bf1f206c33d0173
Murilo Muller <murilomuller@protonmail.com>
2022-12-01 12:02:59 -0300

    atualizado styles do summary

--- dtmoney/src/components/Summary/styles.ts
+++ dtmoney/src/components/Summary/styles.ts
@@ -1,28 +1,32 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const Container = styled.div`
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    gap: 2rem;
-    margin-top: -7rem;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+  margin-top: -7rem;
 
-    div {
-      background: #fff;
-      padding: 1.5rem 2rem;
-      border-radius: 0.25rem;
-      color: #363F5F;
-    
-      header {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-      }
+  .totalSummary {
+    background-color: #33cc95;
+  }
 
-      strong {
-        margin-top: 1rem;
-        font-size: 2rem;
-        font-weight: 600;
-        line-height: 3rem;
-      }
+  div {
+    background: #fff;
+    padding: 1.5rem 2rem;
+    border-radius: 0.25rem;
+    color: #363f5f;
+
+    header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
     }
+
+    strong {
+      margin-top: 1rem;
+      font-size: 2rem;
+      font-weight: 600;
+      line-height: 3rem;
+    }
+  }
 `
