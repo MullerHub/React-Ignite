39df10862c599fb0d41b0a6d3b724ccbdca878da
Murilo Muller <murilomuller@protonmail.com>
2022-12-01 16:05:29 -0300

    Styles from History init

---
+++ Timer/src/pages/History/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const HistoryContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  // color: ${props => props.theme['gray-100']};
+  font-size: 1.125rem;
+  flex-wrap: wrap;
+`
+
+export const HistoryList = styled.div`
+  background-color: red;
+`
