b1c7f9ae449efd5617e294322c56d9f4ed09734a
Murilo Muller <murilomuller@protonmail.com>
2022-12-01 15:45:17 -0300

    ajustes App

--- Timer/src/App.tsx
+++ Timer/src/App.tsx
@@ -1,5 +1,17 @@
-function App() {
-  return <div className="App"></div>
-}
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
-export default App
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
