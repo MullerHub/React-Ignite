aa94ca227eaf44877659203c39a444062e434dc6
Murilo Muller <murilomuller@protonmail.com>
2022-12-06 17:47:09 -0300

    Refactor: ajustes NewCycleForm

--- Timer/src/pages/Home/components/NewCycleForm/index.tsx
+++ Timer/src/pages/Home/components/NewCycleForm/index.tsx
@@ -1,33 +1,13 @@
 import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
 import * as zod from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
-
-const newCycleFormValidationSchema = zod.object({
-  task: zod.string().min(1, 'Informe a tarefa'),
-  minutesAmount: zod
-    .number()
-    .min(5, 'O ciclo precisa ser de no minimo 5 minutos')
-    .max(60, 'O ciclo precisa ser de no maximo 60 minutos')
-})
-
-/*  Tipagem feita de forma manual
-interface INewCicleFormData {
-  task: string
-  minutesAmount: number
-} */
-
-// Tipagem feita automaticamente pelo zod, pegando os formatos do objeto que eu criei "newCycleFormValidationSchema"
-type INewCicleFormData = zod.infer<typeof newCycleFormValidationSchema>
+import { useForm, useFormContext } from 'react-hook-form'
+import { useContext } from 'react'
+import { CyclesContext } from '../..'
 
 export function NewCycleForm() {
-  const { register, handleSubmit, watch, reset } = useForm({
-    resolver: zodResolver(newCycleFormValidationSchema),
-    defaultValues: {
-      task: '',
-      minutesAmount: 0
-    }
-  })
+  const { activeCycle } = useContext(CyclesContext)
+  const { register } = useFormContext()
 
   return (
     <FormContainer>
